148612058c3d796f699ee0519dd118b8008908a3
Christopher Tytone <christyt1806@gmail.com>
2026-05-20 13:38:57 -0400

    fix: update getFriendsList to return empty friend requests object on missing table error

--- src/server/actions/friends.ts
+++ src/server/actions/friends.ts
@@ -39,7 +39,7 @@ export async function getFriendsList(): Promise<Friend[]> {
 
   if (error) {
     if (isMissingTableError(error)) {
-      return [];
+      return { received: [], sent: [] };
     }
 
     throw error;
